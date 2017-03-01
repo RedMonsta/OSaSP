a574bc8381d6859b6b973a4174686c1ebd0a8cda
Vladislav <vambrosevich@gmail.com>
2017-03-01 10:54:03 +0300

    Complete

--- 5.sh
+++ 5.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# 1-name, 2-dir, 3-file
 
-find $2 -type f -printf "%f\n" > /tmp/rez.txt 2> /tmp/error.txt
-find $( readlink -f $2 )  -user $1 -type f -printf "%p  %f  %s\n" > $3 2> /tmp/error.txt
-cat /tmp/rez.txt | wc -l
-sed -r "s,^[^:]+,$(basename $0 -a),"  /tmp/error.txt >&2
-rm /tmp/error.txt /tmp/rez.txt
+if [ $# = 3 ] 
+then
+	find $2 -type f 2> /tmp/errors.txt | wc -l 
+	find $( readlink -f $2 )  -user $1 -type f -printf "%p  %f  %s\n" > $3 2> /tmp/errors.txt
+	sed -r "s/^/$(basename $0 -a): /"  /tmp/errors.txt >&2
+else
+	echo "$(basename $0 -a): Arguments error." > /tmp/errors.txt >&2
+fi
 
+#cat /tmp/rez.txt | wc -l
+#find $2 -type f -printf "%f\n" > /tmp/rez.txt 2> /tmp/error.txt
 #find $( readlink -f "$(find $2 -type f -printf "%f\n" > /tmp/rez.txt 2> /tmp/error.txt )" ) -user $1 -type f -printf "%p  %f  %s\n" > $3
+#sed -r "s,^+,$(basename $0 -a),"  /tmp/error.txt >&2
+#echo $(basename $0 -a)+/tmp/error.txt >&2
+#rm /tmp/rez.txt
+#rm /tmp/errors.txt 
