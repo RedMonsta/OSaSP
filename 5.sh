16ee353e54df991c0d1285063564b28a579d8932
Vladislav <vambrosevich@gmail.com>
2017-03-21 23:25:37 +0300

    Fully completed

--- 5.sh
+++ 5.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # 1-name, 2-dir, 3-file
 
-if [ $# = 3 ] 
+if [ $# == 3 ] 
 then
-	find $2 -type f 2> /tmp/errors.txt | wc -l 
-	find $( readlink -f $2 )  -user $1 -type f -printf "%p  %f  %s\n" > $3 2> /tmp/errors.txt
-	sed -r "s/^/$(basename $0 -a): /"  /tmp/errors.txt >&2
+	find $2 -type f 2> errors.txt | wc -l 
+	find $( readlink -f $2 )  -user $1 -type f -printf "%p  %f  %s\n" > $3 2> errors.txt
+	sed -r "s/^/$(basename $0 -a): /"  errors.txt >&2
+	rm errors.txt
 else
-	echo "$(basename $0 -a): Arguments error." > /tmp/errors.txt >&2
+	echo "$(basename $0 -a): Arguments error." > errors.txt >&2
+	rm errors.txt
 fi
 
 #cat /tmp/rez.txt | wc -l
